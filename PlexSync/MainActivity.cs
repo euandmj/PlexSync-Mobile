6639e4a34add5261995533d62ead6c809cee2674
Euan Jones <euandmj97@gmail.com>
2019-07-26 16:54:56 +0100

    added files

---
+++ PlexSync/MainActivity.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Net.Sockets;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Plugin.Clipboard;
+
+namespace PlexSync
+{
+    // Default View - Send Magnets to Server
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+
+            var clipbrdSender = FindViewById<EditText>(Resource.Id.magnetText);
+            clipbrdSender.Click += this.DumpClipboardString;
+
+            //var btn = FindViewById<Button>(Resource.Id.button1);
+            //btn.Click += delegate
+            //{
+            //    clipbrdSender.Text = 
+            //};
+        }
+
+        private async void DumpClipboardString(object sender, EventArgs e)
+        {
+            // empty the contents of the clipboard into magnetText
+
+            var magnettxt = (EditText)sender;
+
+            string clipboardText = await CrossClipboard.Current.GetTextAsync();
+
+            magnettxt.Text = clipboardText;
+
+#if DEBUG
+            magnettxt.Text = "magnet:?xt=urn:btih:4bf843445b1a19a42dd884f20a5931e42d107b35&dn=South.Park.S22E01.Dead.Kids.720p.WEBRip.AAC2.0.H.264.mkv&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969";
+#endif
+        }
+
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if(drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            // set up the http send and replace the string below with the response
+
+
+            string uri = FindViewById<EditText>(Resource.Id.magnetText).Text;
+
+            if (uri == "") return;
+            string response = string.Empty;
+
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    client.Connect("192.168.0.2", 54000);
+                    client.SendTimeout = 1500;
+                    client.ReceiveTimeout = 1500;
+
+                    var ns = client.GetStream();
+
+                    byte[] data = System.Text.Encoding.ASCII.GetBytes(uri);
+
+                    ns.Write(data, 0, data.Length);
+
+                    data = new byte[1024];
+
+                    // Read the first batch of the TcpServer response bytes.
+                    Int32 bytes = ns.Read(data, 0, data.Length);
+                    response = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                    ns.Close();
+                    client.Close();
+                }
+            }
+            catch (TimeoutException)
+            {
+                response = "Request Timeout";
+            }
+            catch (ArgumentNullException ex)
+            {
+                response = "ArgNull Except: " + ex.Message;
+            }
+            catch (SocketException ex)
+            {
+                response = "Socket Except: " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                response = ex.Message;
+            }
+
+
+
+            View view = (View) sender;
+            Snackbar.Make(view, response, Snackbar.LengthIndefinite)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            if (id == Resource.Id.nav_folder)
+            {
+                Intent next = new Intent(this, typeof(ViewFolder));
+                StartActivity(next);
+            
+            }
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+    }
+}
+
---
+++ PlexSync/ViewFolder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace PlexSync
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar")]
+    public class ViewFolder : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_folder);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            if (id == Resource.Id.nav_magnet)
+            {
+                Intent next = new Intent(this, typeof(MainActivity));
+                StartActivity(next);
+
+            }
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+    }
+}
